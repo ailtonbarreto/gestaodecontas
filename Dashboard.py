be1da260202547e4f3eb916a198325a0c1e6e7e5
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2023-12-23 20:31:24 -0300

    app

--- Dashboard.py
+++ Dashboard.py
@@ -108,7 +108,7 @@ selectboxmeses = [
 
 with col1:
     # st.title("Gestão à Vista",anchor=False)
-    st.image('money-flow.png',use_column_width=True)
+    st.image('money-flow.png',use_column_width=False)
    
 with col5:
     filtro_ano = st.selectbox("Ano", df["Ano"].unique())  
