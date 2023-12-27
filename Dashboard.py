aad30b425aec19eccb3bdf1945c1e8546d9c5d4a
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2023-12-27 20:38:32 -0300

    a

--- Dashboard.py
+++ Dashboard.py
@@ -169,7 +169,7 @@ dfgrafico = dfgrafico.sort_values(by="Valor",ascending=True)
 
 grafico_barras = px.bar(dfgrafico,x="Valor",y="CATEGORIA",
         orientation="h",
-        title=f"Despesas de {filtro_mes} de {filtro_ano}",barmode="group",
+        title=f"Despesas de {filtro_mes} de {filtro_ano}",barmode="stack",
         color_discrete_sequence=["#0081a7"])
 
 #----------------------------------------------------------------------------------------
