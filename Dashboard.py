625cd3c5d1c4b6c4ea4f3b4672e789479775afd8
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2023-12-27 20:59:17 -0300

    updated

--- Dashboard.py
+++ Dashboard.py
@@ -196,6 +196,7 @@ dftabela = df[df["Tipo"] == "ENTRADA"]
 dftabela = dftabela.query('Mês == @filtro_mes & Ano == @filtro_ano')
 dftabela = dftabela.sort_values(by="Status")
 dftabela["Valor"] = dftabela["Valor"].apply(lambda x: f'R$ {x:,.2f}')
+st.write(f"Entrada de {filtro_mes} de {filtro_ano}")
 st.table(dftabela)
  
 
