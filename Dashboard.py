697827156abb9dfc01c8fb191838247af60e240f
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-07 19:13:55 -0300

    a

--- Dashboard.py
+++ Dashboard.py
@@ -147,7 +147,7 @@ grafico_Rosca = px.pie(df_filtrado1,names="Tipo",color='Tipo',category_orders={'
 grafico_Rosca.update_traces(showlegend=False)
 
 
-grafico_colunas = px.area(df_filtrado3,x="Mês",y="Valor",color="Tipo",
+grafico_colunas = px.bar(df_filtrado3,x="Mês",y="Valor",color="Tipo",
         barmode="group",title=f'Entradas e Saídas de {filtro_ano}',category_orders={'Tipo':['SAÍDA','ENTRADA']},
         color_discrete_sequence=["#941b0c","#06d6a0"])
 grafico_colunas.update_yaxes(showgrid=False)
