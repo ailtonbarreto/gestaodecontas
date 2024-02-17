22ebfe769a1566f5b0435a0f8df7fd3aaec07c88
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-17 13:01:16 -0300

    a

--- Dashboard.py
+++ Dashboard.py
@@ -187,7 +187,7 @@ grafico_barras.update_xaxes(showgrid=False,visible=True,title="")
 #Layout gráficos
 
 with col2:
-    st.metric("Entrada",f'R$ {round(df_filtrado4["Valor"].sum(),2):,.2f}↙')
+    st.metric("Entrada",f'R$ {round(df_filtrado4["Valor"].sum(),2):,.2f} ↙')
 with col3:
     st.metric("Saídas",f'R$ {round(df_filtrado2["Valor"].sum(),2):,.2f}')
 with col4:
