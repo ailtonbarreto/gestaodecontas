ef02cabc8fa358459d2dd747d9c18c0760235e33
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-18 18:42:55 -0300

    a

--- Dashboard.py
+++ Dashboard.py
@@ -296,7 +296,7 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 corsaldo = """
     <style>
-    [class="st-emotion-cache-1wivap2 e1i5pmia3"]
+    [class="st-emotion-cache-1xarl3l e1i5pmia1"]
     {
     color: cortexto;
     }
