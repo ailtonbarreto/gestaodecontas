20579f421e526411783134d1d1bd2e0f2e3f0e19
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-20 20:56:03 -0300

    a

--- pages/2 -🔴 Saídas.py
+++ pages/2 -🔴 Saídas.py
@@ -166,17 +166,14 @@ with tab3:
 with tab3:
     col1, col2 = st.columns([1, 10])
 
-    def obter_indices_selecionados(dfeditar):
-        indices_selecionados = []
-        
+    def obter_primeiro_indice_selecionado(dfeditar):
         with col1:
             for index, row in dfeditar.iterrows():
                 if st.checkbox(f'{index}', value=False, key=index):
-                    indices_selecionados.append(index)
-        
-        return indices_selecionados
-    
-    indices_selecionados = obter_indices_selecionados(dfeditar)
+                    return index  # Retorna o primeiro índice marcado
+        return None  # Retorna None se nenhum checkbox estiver marcado
+
+    primeiro_indice_selecionado = obter_primeiro_indice_selecionado(dfeditar)
     
     dftab = dfeditar
     dfeditar = dfeditar.query('index ==@indices_selecionados ')
