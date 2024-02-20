edd64186bb5965d0cdfdff2d8c0391ed5bbc01fa
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-02-19 21:24:46 -0300

    a

--- pages/2 -🔴 Saídas.py
+++ pages/2 -🔴 Saídas.py
@@ -178,7 +178,7 @@ indices_selecionados = obter_indices_selecionados(dfeditar)
 
 dfeditar = dfeditar.query('index ==@indices_selecionados ')
 
-filtro_index=dfeditar.[0]
+filtro_index = dfeditar.index
 
 linha3 = filtro_index+2
 
