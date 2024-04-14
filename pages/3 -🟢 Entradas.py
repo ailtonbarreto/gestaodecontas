be11e5766fef0977f532cd4d5aa692da5e88b2ae
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-04-14 16:29:40 -0300

    updated

--- pages/3 -🟢 Entradas.py
+++ pages/3 -🟢 Entradas.py
@@ -133,7 +133,7 @@ with tab2:
 
     opcoes = dfexcluir.query('Ano == @filtro_ano & Mês == @filtro_mes & Cliente == @filtro_cliente')
     
-    opcoes = opcoes.drop(columns=["Data Vencimento","Data"])
+    opcoes = opcoes.drop(columns=["Data Vencimento","Ano","Mês"])
     
     opcoesdelete = opcoes.index.tolist()
     col1, col2 = st.columns([1, 10])
