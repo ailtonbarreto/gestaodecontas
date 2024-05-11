d4e247141a8af953e764c60e21b4f273d4fdda9b
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-11 19:07:39 -0300

    a

--- pages/3 -🟢 Entradas.py
+++ pages/3 -🟢 Entradas.py
@@ -242,7 +242,7 @@ with tab3:
     dfeditarentrada = dfeditarentrada.drop(columns=["Ano","Mês","Data"])
     dfeditarentrada["Data Vencimento"] = pd.to_datetime(dfeditarentrada["Data Vencimento"]).dt.strftime("%d/%m/%Y")
     with col2:
-        st.table(dfeditarentrada)
+        st.dataframe(dfeditarentrada,use_container_width=True)
 #------------------------------------------------------------------------------------------   
 #Entradas em aberto
 with tab4:
