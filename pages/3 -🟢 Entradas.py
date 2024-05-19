09365d1f6e023c053f5e30245650f8171a3943d4
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-05-18 22:17:52 -0300

    a

--- pages/3 -🟢 Entradas.py
+++ pages/3 -🟢 Entradas.py
@@ -232,34 +232,23 @@ with tab3:
     
     filtro_index = dfeditarentrada
     
-    st.table(filtro_index)
-   
     
+    linha3 = filtro_index+2
 
-    # if not dfeditarentrada.empty:
-    #     filtro_index = dfeditarentrada.index[0]
-    #     print(f"O índice do DataFrame é: {filtro_index}")
-    # else:
-    #     print("O DataFrame está vazio. Nenhum índice pode ser acessado.")
-
+    coluna = 6
     
-    
-    # linha3 = filtro_index+2
+    with tab3:
+        with col2:
+            if st.button("SALVAR EDIÇÃO"):
+                ws1: Worksheet = sh.get_worksheet(0)
+                ws1.update_cell(int(linha3), coluna, editar_status)
+                st.success("Edição salva!")
 
-    # coluna = 6
-    
-    # with tab3:
-    #     with col2:
-    #         if st.button("SALVAR EDIÇÃO"):
-    #             ws1: Worksheet = sh.get_worksheet(0)
-    #             ws1.update_cell(int(linha3), coluna, editar_status)
-    #             st.success("Edição salva!")
-
-    # dfeditarentrada["Valor"] = dfeditarentrada["Valor"].apply(lambda x: f'R$ {x:,.2f}')
-    # dfeditarentrada = dfeditarentrada.drop(columns=["Ano","Mês","Data"])
-    # dfeditarentrada["Data Vencimento"] = pd.to_datetime(dfeditarentrada["Data Vencimento"]).dt.strftime("%d/%m/%Y")
-    # with col2:
-    #     st.table(dfeditarentrada)
+    dfeditarentrada["Valor"] = dfeditarentrada["Valor"].apply(lambda x: f'R$ {x:,.2f}')
+    dfeditarentrada = dfeditarentrada.drop(columns=["Ano","Mês","Data"])
+    dfeditarentrada["Data Vencimento"] = pd.to_datetime(dfeditarentrada["Data Vencimento"]).dt.strftime("%d/%m/%Y")
+    with col2:
+        st.table(dfeditarentrada)
 #------------------------------------------------------------------------------------------   
 #Entradas em aberto
 with tab4:
