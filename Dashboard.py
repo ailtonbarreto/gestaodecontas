2eab622cc78355d9fc35e14269037035248e9231
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2024-08-14 22:55:20 -0300

    a

--- Dashboard.py
+++ Dashboard.py
@@ -235,34 +235,34 @@ grafico_colunas.layout.xaxis.fixedrange = True
 grafico_colunas.layout.yaxis.fixedrange = True
 #----------------------------------------------------------------------------------------
 
-gc = sg.service_account("gestao.json")
-url = 'https://docs.google.com/spreadsheets/d/1HcISrCFCKWOtF6O_RonxH_RVdg2jFBly2KQryc_cZcY/edit?usp=sharing'
-sheet = gc.open_by_url(url)
-dfgrafico = sh.get_worksheet(1)
-dfgrafico = ws.get_all_values()
-dfgrafico = pd.DataFrame(planilha[1:], columns=planilha[0])
+# gc = sg.service_account("gestao.json")
+# url = 'https://docs.google.com/spreadsheets/d/1HcISrCFCKWOtF6O_RonxH_RVdg2jFBly2KQryc_cZcY/edit?usp=sharing'
+# sheet = gc.open_by_url(url)
+# dfgrafico = sh.get_worksheet(1)
+# dfgrafico = ws.get_all_values()
+# dfgrafico = pd.DataFrame(planilha[1:], columns=planilha[0])
 
-dfgrafico['Data'] = pd.to_datetime(dfgrafico["Data Emissão"])
-dfgrafico['Ano'] = dfgrafico['Data'].dt.year
-dfgrafico['Mês'] = dfgrafico['Data'].dt.month
-dfgrafico["Mês"] = dfgrafico["Mês"].apply(determinar_mes)
-dfgrafico = dfgrafico.drop(columns=["Data Emissão","Data"])
-dfgrafico['Valor'] = dfgrafico['Valor'].astype(str)
-dfgrafico['Valor'] = dfgrafico['Valor'].str.replace('.', '').str.replace(',', '.').astype(float)
-dfgrafico = dfgrafico.groupby(["CATEGORIA", "Ano","Mês","Status"])["Valor"].sum().reset_index()
-dfgrafico = dfgrafico.query('Ano == @filtro_ano & Mês == @filtro_mes')
-dfgrafico = dfgrafico.sort_values(by="Valor",ascending=True)
+# dfgrafico['Data'] = pd.to_datetime(dfgrafico["Data Emissão"])
+# dfgrafico['Ano'] = dfgrafico['Data'].dt.year
+# dfgrafico['Mês'] = dfgrafico['Data'].dt.month
+# dfgrafico["Mês"] = dfgrafico["Mês"].apply(determinar_mes)
+# dfgrafico = dfgrafico.drop(columns=["Data Emissão","Data"])
+# dfgrafico['Valor'] = dfgrafico['Valor'].astype(str)
+# dfgrafico['Valor'] = dfgrafico['Valor'].str.replace('.', '').str.replace(',', '.').astype(float)
+# dfgrafico = dfgrafico.groupby(["CATEGORIA", "Ano","Mês","Status"])["Valor"].sum().reset_index()
+# dfgrafico = dfgrafico.query('Ano == @filtro_ano & Mês == @filtro_mes')
+# dfgrafico = dfgrafico.sort_values(by="Valor",ascending=True)
 
-grafico_barras = px.bar(dfgrafico,x="Valor",y="CATEGORIA",text=dfgrafico["Valor"].apply(lambda x: f'R$ {x:,.2f}'),
-        orientation="h",category_orders={'Status':['PAGO','A PAGAR']},
-        title=f"Despesas de {filtro_mes} de {filtro_ano}",color="Status",barmode="stack",
-        color_discrete_sequence=["#0aefff","#ee9b00"])
-grafico_barras.update_yaxes(showgrid=False,visible=True,title="")
-grafico_barras.update_xaxes(showgrid=False,visible=False,title="")
-grafico_barras.layout.xaxis.fixedrange = True
-grafico_barras.layout.yaxis.fixedrange = True
-grafico_barras.update_layout(showlegend=False)
-grafico_barras.update_traces(textfont=dict(size=15,color='#ffffff'),textposition="outside")
+# grafico_barras = px.bar(dfgrafico,x="Valor",y="CATEGORIA",text=dfgrafico["Valor"].apply(lambda x: f'R$ {x:,.2f}'),
+#         orientation="h",category_orders={'Status':['PAGO','A PAGAR']},
+#         title=f"Despesas de {filtro_mes} de {filtro_ano}",color="Status",barmode="stack",
+#         color_discrete_sequence=["#0aefff","#ee9b00"])
+# grafico_barras.update_yaxes(showgrid=False,visible=True,title="")
+# grafico_barras.update_xaxes(showgrid=False,visible=False,title="")
+# grafico_barras.layout.xaxis.fixedrange = True
+# grafico_barras.layout.yaxis.fixedrange = True
+# grafico_barras.update_layout(showlegend=False)
+# grafico_barras.update_traces(textfont=dict(size=15,color='#ffffff'),textposition="outside")
 
 
 #----------------------------------------------------------------------------------------
